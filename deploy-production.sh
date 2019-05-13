0f5cccc9f54c3421442d885f5413ecf18a28009d
annaodziemczyk <43246330+annaodziemczyk@users.noreply.github.com>
2019-05-13 08:01:55 +0100

    Update deploy-production.sh

--- deploy-production.sh
+++ deploy-production.sh
@@ -11,6 +11,8 @@ gcloud --quiet config set container/cluster $CLUSTER_NAME_PRD
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME_PRD
 
+kubectl apply -f ./deployment.yml
+
 gcloud docker -- push gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
 
 yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:latest

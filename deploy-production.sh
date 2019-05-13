5b4f55ffffef16d26bbbc3cc953a3c6d255883c8
annaodziemczyk <43246330+annaodziemczyk@users.noreply.github.com>
2019-05-13 02:02:25 +0100

    Update deploy-production.sh

--- deploy-production.sh
+++ deploy-production.sh
@@ -3,8 +3,8 @@ set -e
 
 docker build -t gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT .
 
-echo $GCLOUD_SERVICE_KEY_PRD | base64 --decode -i > ${HOME}/gcloud-service-key.json
-gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+echo $GCLOUD_SERVICE_KEY_PRD | base64 --decode -i > ${HOME}/client-secret.json
+gcloud auth activate-service-account --key-file ${HOME}/client-secret.json
 
 gcloud --quiet config set project $PROJECT_NAME_PRD
 gcloud --quiet config set container/cluster $CLUSTER_NAME_PRD
